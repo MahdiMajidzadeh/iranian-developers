99c08fadaa220bcc2b7af1cf59ed86981658bd3a
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2019-10-03 13:26:59 +0330

    done

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,15 @@
 $.getJSON("developers.json", function (json) {
-    console.log(json); 
+    $.map(json, function (item) {
+        console.log(item);
+
+        if(item.resume_url.lenth = 0 ){
+            item.resume_url = "#";
+        }
+
+        if(item.personal_url.lenth = 0 ){
+            item.personal_url = "#";
+        }
+
+        $('<div class="col-md-4"><div class="card mb-4 shadow-sm"><div class="card-body"><h3 class="mb-3">' + item.name + '</h3><p class="card-text">در ' + item.work_at + ' کار می کند و در ' + item.skills.join(' ') + ' مهارت دارد.</p><div class="d-flex justify-content-between align-items-center"><div class="btn-group"><a href="'+item.resume_url+'" class="btn btn-sm btn-outline-secondary">رزومه</a><a href="'+item.personal_url+'" class="btn btn-sm btn-outline-secondary">سایت شخصی</a></div></div></div></div></div>').appendTo("#box");
+    });
 });
